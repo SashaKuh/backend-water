3f9c12a518f822f29e24d44dbb8ebd019bb1ccfa
Elizaveta Taran <lizataran103@gmail.com>
2023-12-22 15:51:15 +0200

    add daily rate route

---
+++ controllers/water-controller.js
@@ -0,0 +1,18 @@
+import { ctrlWrapper, httpError } from "../decorators/index.js";
+
+const rateDaily = async (req, res, next) => {
+  /**const {dailyNorma} = req.body
+   * const {id} = req.user
+   *const result = await User.findByIdAndUpdate(id, {dailyNorma})
+    if (!result) {
+    return next(httpError(404, "Not found"));
+   }
+   res.json({
+    message: "Successfully updated"
+   })
+   */
+};
+
+export default {
+  rateDaily: ctrlWrapper(rateDaily),
+};
--- routes/water-route.js
+++ routes/water-route.js
@@ -1,5 +1,8 @@
 import express from "express";
+import waterController from "../controllers/water-controller.js";
 
 const waterRoute = express.Router();
+
+waterRoute.post("/rate", waterController.rateDaily);
 
 export default waterRoute;
