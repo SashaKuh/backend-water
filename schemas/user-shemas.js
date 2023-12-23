5367d2afd6ffcbaf945769ab3f7ca2b0f5740f1e
Rostyslav-Stetsyk <rostyslav.stetsyk1999@gmail.com>
2023-12-23 18:50:02 +0200

    signin done

--- schemas/user-shemas.js
+++ schemas/user-shemas.js
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 const patternForEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
-export const signupUserSchema = Joi.object({
+export const authUserSchema = Joi.object({
   email: Joi.string().required().pattern(patternForEmail).messages({
     "string.pattern.base": "The email must be in the format 'user@email.com'",
     "any.required": "Email is required field",
