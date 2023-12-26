fc97db6742e56dd1366c5d3b13cb672346352ccf
Rostyslav-Stetsyk <rostyslav.stetsyk1999@gmail.com>
2023-12-26 21:05:18 +0200

    fix user avatar object + swagger base + swaggerr doc about /users/signup

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 import express from "express";
 import cors from "cors";
 import logger from "morgan";
+import swager from "swagger-ui-express";
+import fs from "fs/promises";
+import path from "path";
+
+const pathSwagerDocument = path.resolve("./swager.json");
+const swagerDocument = await fs.readFile(pathSwagerDocument, "utf8");
+
 import { usersRoute, waterRoute } from "./routes/index.js";
 
 const app = express();
@@ -9,6 +16,7 @@ const formatLogger = app.get("env");
 app.use(logger(formatLogger));
 app.use(cors());
 app.use(express.json());
+app.use("/docs", swager.serve, swager.setup(JSON.parse(swagerDocument)));
 
 app.use("/users", usersRoute);
 app.use("/water", waterRoute);
